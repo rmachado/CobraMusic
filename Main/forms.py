439dc8b8af13a2d74cb70424d1018d51ae0a92ce
Matias Zanolli <matias@matias-NV57H.(none)>
2012-10-15 20:57:01 -0200

    poca cosa

--- Main/forms.py
+++ Main/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=30, label=u'Usuario')
-    passwd = forms.PasswordInput(label=u'Email address')
+    passwd = forms.PasswordInput()
